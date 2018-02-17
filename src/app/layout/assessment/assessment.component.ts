708233ea09d1be8428e545984dbb2093a02d737e
amit kumar <amit4u25@gmail.com>
2018-02-17 20:39:26 +0530

    Feature: Added functionality to show qustion and sub questions and submit it.

--- src/app/layout/assessment/assessment.component.ts
+++ src/app/layout/assessment/assessment.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { routerTransition } from '../../router.animations';
 import { Router } from '@angular/router';
 import { HttpClientHelper } from '../../app.httpClient';
+import index from '@angular/cli/lib/cli';
 @Component({
     selector: 'app-form',
     templateUrl: './assessment.component.html',
@@ -16,8 +17,7 @@ export class AssessmentComponent implements OnInit {
     subIndexSecondArray: any = [];
     resultContents; any = [];
     selectValue: any = [];
-    subContentsResult: any [];
-    tempQ: any;
+    subContentsResult: any = [];
     qustions: any = [];
     constructor(private router: Router,
                 private application: HttpClientHelper) {
@@ -45,41 +45,30 @@ export class AssessmentComponent implements OnInit {
 
     ngOnInit() {}
 
-    onSelectionChange(entry, index) {
 
-        if (this.indexArray.length > 0) {
-            let id = this.indexArray.indexOf(index);
-            if (id === -1) {
-                this.answerArray.push(entry);
-                this.indexArray.push(index);
-            } else {
-                this.answerArray.splice(id, 1);
-                this.indexArray.splice(id, 1);
-                this.answerArray.push(entry);
-                this.indexArray.push(index);
-            }
-        } else {
-            this.answerArray.push(entry);
-            this.indexArray.push(index);
-        }
-
-        if (entry) {
-            this.qustions[index].isType = true;
-        } else {
-            this.qustions[index].isType = false;
-        }
-    }
 
     submit() {
 
-        let dummy = [];
+        let answer = [];
         for (let i = 0; i < this.indexArray.length; i++) {
             let input = {
-                index: this.indexArray[i],
+                index: this.indexArray[i] + 1,
                 answer: this.answerArray[i]
             };
-            dummy.push(input);
+            answer.push(input);
         }
+
+        let subAnswer = [];
+        for (let j = 0; j < this.subIndexFirstArray.length; j++) {
+            let data = {
+                firstIndex: this.subIndexFirstArray[j] + 1,
+                secondIndex: this.subIndexSecondArray[j] + 1,
+                answer: this.subAnswerArray[j],
+            };
+            subAnswer.push(data);
+        }
+        console.log(answer);
+        console.log(subAnswer);
     }
 
     back() {
@@ -93,40 +82,75 @@ export class AssessmentComponent implements OnInit {
                 item.tierTwoQuestion.forEach(thirdItem => {
                     thirdItem.isType = false;
                     this.qustions = item.tierTwoQuestion;
-                }
-                setTimeout(() => {
-                    console.log(this.qustions);
-                }, 200);
-
+                });
             }
         });
     }
 
-    onSecondSelectionChange(status, index1, index2) {
-        console.log(status);
-        console.log(index1);
-        console.log(index2);
+    onSelectionChange(entry, index) {
 
-        if (this.subIndexFirstArray.length > 0) {
-            let id = this.subIndexFirstArray.indexOf(index1);
+        if (this.indexArray.length > 0) {
+            let id = this.indexArray.indexOf(index);
+            if (id === -1) {
+                this.answerArray.push(entry);
+                this.indexArray.push(index);
+            } else {
+                this.answerArray.splice(id, 1);
+                this.indexArray.splice(id, 1);
+                this.answerArray.push(entry);
+                this.indexArray.push(index);
+
+                for(let i = 0; i < this.subIndexFirstArray.length; i++) {
+                    let temp = this.subIndexFirstArray.indexOf(index);
+                    if(temp !== -1) {
+                        this.subIndexFirstArray.splice(temp, 1);
+                        this.subIndexSecondArray.splice(temp, 1);
+                        this.subAnswerArray.splice(temp, 1);
+                        this.subContentsResult.splice(temp, 1);
+                    }
+                }
+            }
+        } else {
+            this.answerArray.push(entry);
+            this.indexArray.push(index);
+        }
+
+        if (entry) {
+            this.qustions[index].isType = true;
+        } else {
+            this.qustions[index].isType = false;
+        }
+    }
+
+    onSecondSelectionChange(status, index1, index2) {
+
+
+        let value = index1.toString() + '.' + index2.toString();
+
+        if (this.subAnswerArray.lenght > 0) {
+            let id = this.subContentsResult.indexOf(value);
             if (id === -1) {
                 this.subAnswerArray.push(status);
                 this.subIndexFirstArray.push(index1);
+                this.subIndexSecondArray.push(index2);
+                this.subContentsResult.push(value);
             } else {
                 this.subAnswerArray.splice(id, 1);
                 this.subIndexFirstArray.splice(id, 1);
+                this.subIndexSecondArray.splice(id, 1);
+                this.subContentsResult.splice(id, 1);
+
                 this.subAnswerArray.push(status);
                 this.subIndexFirstArray.push(index1);
+                this.subIndexSecondArray.push(index2);
+                this.subContentsResult.push(value);
             }
+
         } else {
             this.subAnswerArray.push(status);
             this.subIndexFirstArray.push(index1);
             this.subIndexSecondArray.push(index2);
-            this.subContentsResult.push({
-                status: status,
-                indexFirst: index1,
-                indexSecond: index2,
-            });
+            this.subContentsResult.push(value);
         }
     }
 }
